4ce8ce6a36cac7f9d5f4be9e22aec12a3816f1dd
tnguyen98 <32310508+tnguyen98@users.noreply.github.com>
2020-10-01 08:54:41 -0400

    Added movie details page, movie grid view, and tab bar nav

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Timothy Nguyen on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
